8c9e9cba1ad197ff8ad83d0cea634e09cbbe2d63
Use1rxd <152656172+Use1rxd@users.noreply.github.com>
2023-12-01 23:51:38 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,8 @@
+echo off
+title User Sohbet #Geliyoruz
+color c
+cls
+:a
+node index.js
+goto a
+pause
---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "MTE4MDIyMDAyMjIyOTA0OTQwNA.G6z7W-.lRdeT8dKBbXBETSta9PIFKhnpqKN3Igk5mFGi0", // botun tokeni
+    prefix: ".", // prefixi
+    isim_emoji: "|", // isim ve yaşın arasına gelecek emoji
+    anonim_chat_kategori: "1180220259467264141",
+    rapor_channel: "1180217217133052044",
+    anonim_log: "1180217217133052044",
+    anonim_yetkili: "1180217213731479710"
+}
